7d0a2dc186d8d592428e18098ad9d693d40d4dc3
topdawg116 <esteban.m.eliza@gmail.com>
2015-05-08 22:38:17 -0400

    Start
    First Sub

---
+++ Boats and Hoes/Assets/Tile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Game;
+
+public class Tile : MonoBehaviour 
+{
+	Side[] m_sides;
+	int m_id;
+	string m_textureName;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_sides = new Side[4];
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
